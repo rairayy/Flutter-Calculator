b7a2c84e64092c739a367c38de270f14fb25ee90
Riana Lim <limriana9@gmail.com>
2020-02-27 18:36:53 +0800

    updated calculator design

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyCalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: Calculator(),
     );
   }
@@ -28,79 +28,87 @@ class Calculator extends StatefulWidget {
   @override
   _CalculatorState createState() => _CalculatorState();
 }
-
+double size;
 class _CalculatorState extends State<Calculator> {
-  String output = "0";
-  int a = -1, b = -1;
-  String op = "None";
+  String output = "0", full = "", op = "", a_str = "", b_str = "";
 
   @override
-  Widget build(BuildContext context) { // creating the widget
+  Widget build(BuildContext context) {
+    size = (MediaQuery.of(context).size.width / 4)- 2;
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Riana Does Maths")
-      ),
-      body: new Container( //defines the content of the body
+      backgroundColor: Color.fromARGB(255, 19, 46, 71),
+      body: new Container(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             // displaying stuff
             Container(
-              color: Colors.transparent,
-              child: new Container(
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.all(20),
-                constraints: BoxConstraints.expand(
-                  height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 100.0,
-                ),
-                decoration: new BoxDecoration(
-                  color: new Color.fromRGBO(113, 128, 93, 100),
-                  borderRadius: new BorderRadius.all(Radius.circular(10))
-                ),
+              margin: EdgeInsets.only( top: 50, bottom:20 ),
+                padding: EdgeInsets.only(right: 20, top: 20),
+                constraints: BoxConstraints.expand(height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 100.0),
+                decoration: new BoxDecoration(color: Colors.black45),
                 alignment: Alignment.bottomRight,
-                child: Text("$output",
-                  style: TextStyle(
-                    fontSize: 50.0,
-                    color: Colors.black54
-                  ),
-                  textAlign: TextAlign.right,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("$full",
+                      style: TextStyle(
+                        fontFamily: 'GoogleSansRegular',
+                        color: Colors.white70,
+                        fontSize: 30
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                    Text("$output",
+                      style: TextStyle(
+                        fontFamily: 'GoogleSansRegular',
+                        color: Colors.white,
+                        fontSize: 50
+                      ),
+                      textAlign: TextAlign.right,
+                    )
+                  ],
+                )
+            ),
+
+            // buttons
+            Column(
+              children: <Widget>[
+                Row( 
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _button("7", _setval, 7, false),
+                    _button("8", _setval, 8, false),
+                    _button("9", _setval, 9, false),
+                    _button("+", _arg, 0, true)
+                  ],
                 ),
-              )
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("7", _setval, 7),
-                _button("8", _setval, 8),
-                _button("9", _setval, 9),
-                _button("+", _arg, 0)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("4", _setval, 4),
-                _button("5", _setval, 5),
-                _button("6", _setval, 6),
-                _button("-", _arg, 1)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("1", _setval, 1),
-                _button("2", _setval, 2),
-                _button("3", _setval, 3),
-                _button("*", _arg, 2)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("C", _clear, 0),
-                _button("0", _setval, 0),
-                _button("=", _disp, 0),
-                _button("/", _arg, 3)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _button("4", _setval, 4, false),
+                    _button("5", _setval, 5, false),
+                    _button("6", _setval, 6, false),
+                    _button("-", _arg, 1, true)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _button("1", _setval, 1, false),
+                    _button("2", _setval, 2, false),
+                    _button("3", _setval, 3, false),
+                    _button("×", _arg, 2, true)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    _button("C", _clear, 0, true),
+                    _button("0", _setval, 0, false),
+                    _button("=", _disp, 0, true),
+                    _button("÷", _arg, 3, true)
+                  ],
+                )
               ],
             )
           ],
@@ -111,12 +119,14 @@ class _CalculatorState extends State<Calculator> {
 
   void _clear(int val) {
     setState((){
-      a = b = -1;
+      a_str = b_str = full = "";
       output = "0";
     });
   }
 
   void _disp(int val) {
+    int a = int.parse(a_str);
+    int b = int.parse(b_str);
     if( a != -1 && b != -1 ) {
       int ans = 0;
       if( op == "add") ans = a+b;
@@ -125,50 +135,63 @@ class _CalculatorState extends State<Calculator> {
       else if( op == "div") ans = a~/b;
       setState((){
         output = ans.toString();
-        a = b = -1;
+        a_str = b_str = full = "";
       });
     }
   }
 
   void _arg(int val) {
-    String arg = "";
-    if( val == 0 ) arg = "add";
-    else if( val == 1 ) arg = "sub";
-    else if ( val == 2 ) arg = "mul";
-    else if ( val == 3 ) arg = "div";
+    String arg = "", cmd = "";
+    if( val == 0 ) {arg = "add"; cmd = "+";}
+    else if( val == 1 ) {arg = "sub"; cmd = "-";}
+    else if ( val == 2 ) {arg = "mul"; cmd = "×";}
+    else if ( val == 3 ) {arg = "div"; cmd = "÷";}
     setState((){
       op = arg;
+      full += " "+cmd+" ";
+      output = "";
     });
   }
 
   void _setval(int val) {
-    if( a == -1 ) {
+    String val_str = val.toString();
+    if( op == "" ) {
       setState((){
-        a = val;
-        output = a.toString();
+        a_str += val_str;
       });
-    } else if ( a != -1 && op != "None" ) {
+    } else {
       setState((){
-        b = val;
-        output = b.toString();
+        b_str += val_str;
       });
     }
+    if( output == "0" ) setState((){output = val_str;});
+    else setState((){output += val_str;});
+    setState((){full += val_str;});
   }
 }
 
-Widget _button (String number, Function(int) f, int val) {
-  return MaterialButton(
-    shape: new CircleBorder(),
-    height: 100.0,
-    minWidth: 100.0,
-    child: Text(number,
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)
-    ),
-    textColor: Colors.white,
-    color: Colors.black45,
-    onPressed: () {
-      print(val);
-      f(val);
-    } // to pay respect
+Widget _button (String number, Function(int) f, int val, bool func ) {
+  Color clr = Colors.white;
+  if( func ) clr = Color.fromARGB(255, 32, 96, 128);
+  else clr = Color.fromARGB(255, 96, 96, 96);
+  return Container(
+    padding: EdgeInsets.all(2),
+    width: size,
+    height: size,
+    child: RaisedButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      elevation: 2,
+      child: Text(number,
+        style: TextStyle(
+          fontFamily: 'GoogleSansRegular',
+          fontSize: 40.0
+        )
+      ),
+      textColor: Colors.white,
+      color: clr,
+      onPressed: () {
+        f(val);
+      }
+    )
   );
 }
